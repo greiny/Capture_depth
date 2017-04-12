c7ccfe6aa8566a9ccb2ad6c39dd3b19745ad6f9f
greiny <greiny@naver.com>
2017-04-12 14:43:47 +0900

    save image and mat

--- main.cpp
+++ main.cpp
@@ -28,23 +28,15 @@ int main()
     // Creating OpenCV matrix from IR image
     Mat ir(Size(640, 480), CV_8UC1, (void*)dev->get_frame_data(rs::stream::infrared), Mat::AUTO_STEP);
 
-    // Display the image in GUI
-    namedWindow("Display Image_1", WINDOW_AUTOSIZE );
-    imshow("Display Image_!", ir);
-
     // Apply Histogram Equalization
     equalizeHist( ir, ir );
-
-    // Display the image in GUI
-    namedWindow("Display Image_2", WINDOW_AUTOSIZE );
-    imshow("Display Image_2", ir);
-
+    cout << "ir = "<< endl << " "  << ir << endl << endl;
     applyColorMap(ir, ir, COLORMAP_JET);
-   cout << "it = "<< endl << " "  << it << endl << endl;
+    imwrite( "depth_Image.jpg", ir);
 
     // Display the image in GUI
-    namedWindow("Display Image_3", WINDOW_AUTOSIZE );
-    imshow("Display Image_3", ir);
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", ir);
 
     waitKey(0);
 
